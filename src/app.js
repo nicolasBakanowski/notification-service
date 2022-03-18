9a4a056ec14bc63086742a9d197f721ccb38a3fe
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2022-03-17 22:36:03 -0300

    add model otp, add db instance, create sequelize with configs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const httpStatus = require('http-status');
+const helmet = require('helmet');
 const sendWhatsApp = require("./integration/events/whatsapp-event/whatsapp-event");
 
-sendWhatsApp()
+const app = express();
+// set security HTTP headers
+app.use(helmet());
 
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+
+// gzip compression
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
+
+sendWhatsApp()
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+    })
 console.log('running')
