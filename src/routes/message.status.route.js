9752f5384b32d463b89c04bdee428d15a68943f4
i.arzaut <ignacio.arzaut@surfdevelopment.tech>
2022-03-24 17:26:36 -0300

    fix typo

--- src/routes/message.status.route.js
+++ src/routes/message.status.route.js
@@ -3,19 +3,26 @@ const express = require('express');
 const createLog = require('../utils/createLog.js')
 const httpStatus = require('http-status')
 const router = express.Router();
-const StatusSchema = require('../validations/status.validation')
+const statusSchema = require('../validations/status.validation')
 
 // THIS ENDPOINT RECEIVES A REQUEST FROM TWILIO WEBHOOK WITH NOTIFICATION DELIVERY STATUS
 
 router.route('/')
   .post( async ( req, res ) => {
-    const status = StatusSchema.validate({ ...req.body })
-    status.error && res.status( httpStatus.BAD_REQUEST ).send({ errorMessage: status.error })
+    const status = statusSchema.validate({ ...req.body })
+    status.error && 
+      res
+        .status(httpStatus.BAD_REQUEST)
+        .send({ errorMessage: status.error })
     try {
       const log = await createLog(status)
-      return res.status( httpStatus.CREATED ).send({ message: "Notification was succesfully logged.", log: log})
+      return res
+        .status(httpStatus.CREATED)
+        .send({ message: "Notification was succesfully logged.", log: log})
     } catch (err) {
-      return res.status( httpStatus.INTERNAL_SERVER_ERROR ).send({message: "There was an error and the notification was not logged properly.", errorCode: err.code})
+      return res
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .send({message: "There was an error and the notification was not logged properly.", errorCode: err.code})
     }
   })
 
